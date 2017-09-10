5e300d05ce622e413867b3203dea289e764ecc3c
Abhishek Ravi <abhishekravi@InnovationM-30-Ravi.local>
2017-09-10 18:53:30 +0530

    Added File

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const http = require('http');
+// const http = require('http');
+//
+// // Constants
+// const hostName = "127.0.0.1";
+// const portNumber = 7073;
+//
+// // Create Server
+// const server = http.createServer(function(req, res){
+//
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello Green Syntax");
+// });
+//
+// server.listen(portNumber, function(){
+//
+//   console.log("Server Started... ");
+// });
 
-// Constants
-const hostName = "localhost";
-const portNumber = 7071;
-
-// Create Server
-const server = http.createServer(function(req, res){
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello Green Syntax");
-});
-
-server.listen(portNumber, hostName, function(){
-
-  console.log("Server Started at ".portNumber);
-});
+console.log("Hello green");
