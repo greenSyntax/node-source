eb221ac74a1cc1387bac561dfbef94ee513094e8
greenSyntax <ab.abhishek.ravi@gmail.com>
2017-10-06 02:42:01 +0530

    Added read_query.js

--- read_query.js
+++ read_query.js
@@ -3,8 +3,9 @@ var url = require('url');
 var urlAddress = "http://google.com/index.html?page=1&post=12";
 
 var path = url.parse(urlAddress, true);
-console.log(path.hostname);
-console.log(path.pathname);
-console.log(path.search);
-console.log(path.query.page);
-console.log(path.query.post);
+
+console.log(path.hostname);   //googel.com
+console.log(path.pathname);   // /index.html
+console.log(path.search);     //?page=1&post=12
+console.log(path.query.page); //1
+console.log(path.query.post); //12
